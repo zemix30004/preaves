93b9a594b8309ea0ad6a5bb2ec5979a617ffdd61
Eugene Zavoloka <eugenezm@gmail.com>
2021-11-09 20:57:20 +0200

    initial commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,25 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Brand</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">Orders</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Submit</button>
+        </form>
+        <ul class="navbar-nav navbar-right">
+            <li><a href="#" class="">Cart(0)</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
